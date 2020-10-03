e50cb44eda4dfe4df85aef8c7c5cb8fee5822f60
Timothy Coish <tdcoish@gmail.com>
2020-10-02 22:58:43 -0700

    Lotta articles

--- Articles/jsArticleTextFormatting.js
+++ Articles/jsArticleTextFormatting.js
@@ -10,18 +10,20 @@
 function formatArticleText(){
     var articleContent = document.getElementsByClassName("articleContent");
     var articleRawTextString = articleContent[0].innerHTML;
-
     articleRawTextString = articleRawTextString.replace(/\n/g, "<br><br>");
-
     articleRawTextString = articleRawTextString.replace(/<br><br><br><br>/g, "<br><br>");
-
-    console.log(articleRawTextString);
+    // console.log(articleRawTextString);
 
     // using the global find replace now.
     articleRawTextString = articleRawTextString.replace(/TDC_ARTICLE_START/g, "<div class=\"articleSnippet\">");
     articleRawTextString = articleRawTextString.replace(/TDC_ARTICLE_STOP/g, "</div>");
-
-    articleContent[0].innerHTML = articleRawTextString;
+  
+    articleContent[0].innerHTML = articleRawTextString; 
+    
+    // have to make the page title (on the tab) the same as the article title.
+    var articlePageTitleText = document.getElementById("pageTitle");
+    var articleTitleText = document.getElementById("articleTitle");
+    articlePageTitleText.innerHTML = articleTitleText.innerHTML;
 }
 
 
